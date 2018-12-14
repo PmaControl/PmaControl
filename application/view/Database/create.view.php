a577f0291f96a02aed5255c59933e324d49a7993
Aurélien LEQUOY <aurelien@pmacontrol.com>
2018-12-14 12:06:54 +0100

    fixed topology with DB and add new stuff for cluster

--- application/view/Database/create.view.php
+++ application/view/Database/create.view.php
@@ -6,55 +6,102 @@
  */
 
 use Glial\Html\Form\Form;
-
 ?>
 
-<form action="" method="POST">
+
+
+
+<?php
+if (!empty($data['compte'])) {
+    echo '<ul class="list-group">';
+
+    foreach ($data['compte'] as $compte) {
+        echo '<li class="list-group-item">'.$compte.'</li>';
+    }
+    echo '</ul>';
+}
+?>
+
+
+<form action="<?= LINK ?>database/create" method="POST">
     <div class="panel panel-primary">
         <div class="panel-heading">
 
-            <h3 class="panel-title"><?= __('Rename database to') ?></h3>
+            <h3 class="panel-title"><?= __('Create database') ?></h3>
         </div>
 
         <div class="well">
             <div class="row">
-                <div class="col-md-3">
-                    <?php
-                    echo __("Server")."<br />";
+                <div class="col-md-6">
+<?php
+echo __("Server")."<br />";
 
-                    \Glial\Synapse\FactoryController::addNode("Common", "getSelectServerAvailable", array("rename", "id_mysql_server","100%"));
+\Glial\Synapse\FactoryController::addNode("Common", "getSelectServerAvailable", array("database", "id_mysql_server", array("data-width" => "auto", "multiple" => "multiple")));
 
-
-
-                    echo '</div><div class="col-md-3">';
-
-
-                    echo __("Database")."<br />";
-                    $data['listdb1'] = array();
-
-                    echo Form::select("rename", "database", $data['listdb1'], "", array("data-live-search" => "true", "class" => "selectpicker","data-width"=>"100%"));
+echo '</div>';
+echo '</div>';
 
 
-
-                    echo '</div><div class="col-md-3">';
-
-                    echo __('Rename database to');
-                    echo Form::input("rename", "new_name", array("class" => "form-control"));
+echo '<div class="row">';
+echo '&nbsp;';
+echo '</div>';
 
 
-                    echo '</div><div class="col-md-3">';
+echo '<div class="row">';
+
+echo '<div class="col-md-3">';
 
 
-                    echo '<div class="form-group"><br />
-    <div class="checkbox checbox-switch switch-success">
-        <label>
-            '.Form::input("rename", "adjust_privileges", array("class" => "form-control", "type" => "checkbox", "checked" => "checked")).'
-            <span></span>
-            '.__('Adjust privileges').'
-        </label>
-    </div>
-</div>';
-                    ?>
+echo __("User name")."<br />";
+$data['listdb1'] = array();
+
+
+echo Form::input("database", "user", array("class" => "form-control"));
+
+
+echo '</div>';
+
+echo '<div class="col-md-1">';
+echo '&nbsp;';
+
+echo Form::input("database", "gg", array("class" => "form-control", "value" => "@", "readonly" => "readonly"));
+echo '</div>';
+
+
+
+echo '<div class="col-md-3">';
+
+echo __('Hostname');
+echo Form::input("database", "hostname", array("class" => "form-control"));
+
+
+echo '</div><div class="col-md-3">';
+echo __('Global privileges');
+echo Form::select("database", "id_mysql_privilege", $data['mysql_privilege'], "",
+    array("class" => "form-control", "class" => "selectpicker",
+    "data-live-search" => "true", "data-width" => "100%", "multiple" => "multiple"));
+
+echo '</div>';
+echo '</div>';
+
+echo '<div class="row">';
+echo '&nbsp;';
+echo '</div>';
+
+echo '<div class="row">';
+
+
+
+echo '<div class="col-md-12">';
+echo __('Databases to create (separated by coma)');
+
+
+echo Form::input("database", "name", array("class" => "form-control", "autocomplete" => "off", "width" => "auto"));
+?>
+
+
+
+
                 </div>
             </div>
 
