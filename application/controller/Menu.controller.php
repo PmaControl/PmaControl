272d0d6f3220ef8fb00f99033bd59674634ac09b
Aurélien LEQUOY <aurelien@pmacontrol.com>
2019-01-29 18:34:47 +0100

    correction du menu (multi niveau et non prise en compte de la racine

--- application/controller/Menu.controller.php
+++ application/controller/Menu.controller.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,18 +10,19 @@ use \Glial\Synapse\FactoryController;
 
 class Menu extends Controller
 {
+
     public function show($params)
     {
         $id_menu = $params[0];
         //debug($id_menu);
 
-        $db = $this->di['db']->sql(DB_DEFAULT);
-        $sql = "SELECT b.*, MAX(c.id) AS dropdown FROM `menu_group` a
+        $db           = $this->di['db']->sql(DB_DEFAULT);
+        $sql          = "SELECT b.*, MAX(c.id) AS dropdown FROM `menu_group` a
 INNER JOIN `menu` b ON a.id = b.group_id
 LEFT JOIN `menu` c ON b.bg < c.bg AND b.bd > c.bd AND c.active = 1
-WHERE b.active = 1 and b.parent_id is not null and a.id='" . $id_menu . "' GROUP BY b.id "
-                . "ORDER BY b.bg";
-        $data['sql'] = $sql;
+WHERE b.active = 1 and b.parent_id is not null and a.id='".$id_menu."' GROUP BY b.id "
+            ."ORDER BY b.bg";
+        $data['sql']  = $sql;
         $data['menu'] = $db->sql_fetch_yield($sql);
 
         switch ($id_menu) {
@@ -37,9 +37,9 @@ WHERE b.active = 1 and b.parent_id is not null and a.id='" . $id_menu . "' GROUP
                 $data['position'] = "top";
                 break;
         }
-        
+
         $data['selectedmenu'] = $this->getSelectedLevelOneMenu($id_menu);
-        
+
         $this->set('data', $data);
     }
 
@@ -48,18 +48,13 @@ WHERE b.active = 1 and b.parent_id is not null and a.id='" . $id_menu . "' GROUP
         $Array = FactoryController::getRootNode();
 
         $db = $this->di['db']->sql(DB_DEFAULT);
-        $sql = "SELECT IFNULL(e.id,IFNULL(d.id,IFNULL(c.id,IFNULL(b.id,a.id)))) AS levelonemenu_id "
-                . "FROM `menu` AS a "
-                . "LEFT JOIN `menu` AS b ON a.parent_id = b.id AND b.parent_id IS NOT NULL "
-                . "LEFT JOIN `menu` AS c ON b.parent_id = c.id AND c.parent_id IS NOT NULL "
-                . "LEFT JOIN `menu` AS d ON c.parent_id = d.id AND d.parent_id IS NOT NULL "
-                . "LEFT JOIN `menu` AS e ON d.parent_id = e.id AND e.parent_id IS NOT NULL "
-                . "WHERE a.group_id= " . $id_menu . " AND a.class = '".$Array[0]."' AND a.method = '".$Array[1]."'";
-        $ThisData = $db->sql_fetch_all($sql);
-        
-        if (isset($ThisData[0]["levelonemenu_id"]))
-            return $ThisData[0]["levelonemenu_id"];
-        else
-            return null;
+        $sql = "WITH a as (SELECT bg,bd, group_id FROM menu where `class`='".$Array[0]."' AND `method` = '".$Array[1]."' AND parent_id is not null LIMIT 1)
+            SELECT * FROM menu b,a WHERE b.bg <= a.bg AND b.bd >= a.bg AND a.group_id = b.group_id AND parent_id is not null ORDER by b.bg";
+
+        $res = $db->sql_query($sql);
+
+        while ($ob = $db->sql_fetch_object($res)) {
+            return $ob->id;
+        }
     }
 }
--- application/view/Menu/show.view.php
+++ application/view/Menu/show.view.php
@@ -14,74 +14,75 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="<?=LINK ?>home/index" style="color:#fff"><i class="fa fa-database fa-lg"></i> <?=SITE_NAME ?> <span class="badge badge-info" style="font-variant: small-caps; font-size: 14px; vertical-align: middle; background-color: #4384c7" title="<?=SITE_LAST_UPDATE ?>"><?=SITE_VERSION ?> (<?=SITE_LAST_UPDATE ?>)</span></a>
+                    <a class="navbar-brand" href="<?= LINK ?>home/index" style="color:#fff"><i class="fa fa-database fa-lg"></i> <?= SITE_NAME ?> <span class="badge badge-info" style="font-variant: small-caps; font-size: 14px; vertical-align: middle; background-color: #4384c7" title="<?= SITE_LAST_UPDATE ?>"><?= SITE_VERSION ?> (<?= SITE_LAST_UPDATE ?>)</span></a>
                 </div>
                 <?php
             endif;
             ?>
 
-            
+
             <?php
-            
-            $class="";
-            if ($data['position'] === "bottom")
-            {
-                $class=" pull-right";
+            $class = "";
+            if ($data['position'] === "bottom") {
+                $class = " pull-right";
             }
             ?>
 
-            <div class="collapse navbar-collapse bs-example-js-navbar-collapse<?=$class ?>">
+            <div class="collapse navbar-collapse bs-example-js-navbar-collapse<?= $class ?>">
                 <ul class="nav navbar-nav">
                     <?php
                     $close_at = [];
-                    $i = 1;
+                    $i        = 1;
 
                     foreach ($data['menu'] as $item) {
 
                         foreach ($close_at as $key => $to_close) {
                             if ($item['bg'] > $to_close) {
-                                echo '
-                                </ul>
-                                </li>' . "\n";
+                                echo '</ul>
+                                </li>'."\n";
                                 unset($close_at[$key]);
                             }
                         }
 
-                        if ( ($item['bd'] - $item['bg'] > 1) && ( !empty($item['dropdown']) ) ) {
+
+                        $active = '';
+                        if ($data['selectedmenu'] == $item["id"]) {
+                            $active = ' active';
+                        }
+
+                        //debug($data['selectedmenu']);
+                        //debug($item);
+
+
+                        if (($item['bd'] - $item['bg'] > 1) && (!empty($item['dropdown']) )) {
                             echo '
-                                <li class="dropdown';
-                            if($data['selectedmenu']==$item["id"])
-                                echo ' active';
-                            echo '">
-                                <a id="drop' . $i . '" href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" role="button" aria-expanded="false">
-                                ' . $item['icon'] . ' ' . __($item['title']) . '
+                                <li class="dropdown'.$active.'">
+                                <a id="drop'.$i.'" href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" role="button" aria-expanded="false">
+                                '.$item['icon'].' '.__($item['title']).'
                                 <span class="caret"></span>
                                 </a>
-                                <ul class="dropdown-menu" role="menu" aria-labelledby="drop' . $i . '">';
+                                <ul class="dropdown-menu" role="menu" aria-labelledby="drop'.$i.'">';
 
                             $close_at[] = $item['bd'];
                             $i++;
                         } else {
-                                $item['url'] = str_replace('[IMG]', IMG, $item['url']);
-                                $item['icon'] = str_replace('[IMG]', IMG, $item['icon']);
-
-                                
-                                $item['url'] = str_replace(array('{LINK}'), array(LINK), $item['url']);
-
-                                if (strstr($item['url'],'{PATH}'))
-                                {
-                                    $item['url'] = WWW_ROOT .str_replace('{PATH}', '', $item['url']).'/'.substr($_GET['glial_path'], 3);
-                                }
+                            $item['url']  = str_replace('[IMG]', IMG, $item['url']);
+                            $item['icon'] = str_replace('[IMG]', IMG, $item['icon']);
 
 
-                                $PATH = WWW_ROOT .substr($_GET['glial_path'], 3);
+                            $item['url'] = str_replace(array('{LINK}'), array(LINK), $item['url']);
+
+                            if (strstr($item['url'], '{PATH}')) {
+                                $item['url'] = WWW_ROOT.str_replace('{PATH}', '', $item['url']).'/'.substr($_GET['glial_path'], 3);
+                            }
 
 
-                            echo '<li role="presentation"><a role="menuitem" tabindex="-1" href="' . $item['url'] . '">' . $item['icon'] . ' ' . __($item['title']) . '</a></li>';
+                            $PATH = WWW_ROOT.substr($_GET['glial_path'], 3);
+
+
+                            echo '<li class="'.$active.'" role="presentation"><a role="menuitem" tabindex="-1" href="'.$item['url'].'">'.$item['icon'].' '.__($item['title']).'</a></li>';
                         }
                     }
-
-
                     ?>
 
 
