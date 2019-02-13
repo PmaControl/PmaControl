ff74fc1665452c49cc822555caa85bff725d3f1e
Aurélien LEQUOY <aurelien@pmacontrol.com>
2019-02-13 19:03:01 +0100

    ajout de la premiere version des jobs (2)

--- tmp/b53d8187-427d-4bde-83dc-1e8be61699fc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-/usr/bin/php7.2 /data/www/pmacontrol/application/webroot/index.php Database databaseRefresh 84 1 'drupal_home' '/mysql/backup' >> /data/www/pmacontrol/tmp/log/Database-refresh-5c645ad816ef3.log
-/usr/bin/php7.2 /data/www/pmacontrol/application/webroot/index.php job callback b53d8187-427d-4bde-83dc-1e8be61699fc
