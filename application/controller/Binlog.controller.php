8437526326223434c83849421fb591a7d62d8e60
68Koncept <timansl@hotmail.com>
2019-05-10 20:23:55 +0200

    add binlog search and backup

--- application/controller/Binlog.controller.php
+++ application/controller/Binlog.controller.php
@@ -8,19 +8,57 @@
 use \Glial\Synapse\Controller;
 use \App\Library\Debug;
 use App\Library\Extraction;
+use App\Library\Mysql;
 
 class Binlog extends Controller
 {
 
     public function index()
     {
+
+
         $data = array();
         $this->set('data', $data);
     }
 
     public function add()
     {
-        $sql = "INSSERT INTO ";
+        $this->di['js']->addJavascript(array('Binlog/index.js'));
+
+        //debug($_POST);
+
+        if ($_SERVER['REQUEST_METHOD'] === "POST") {
+
+            if (!empty($_POST['binlog_max']['size'])) {
+
+                preg_match('/[kmgKMG]$/', $_POST['binlog_max']['size'], $output_array);
+
+                if (!empty($output_array[0])) {
+
+                    $number = substr($_POST['binlog_max']['size'], 0, -1);
+
+                    switch (strtolower($output_array[0])) {
+                        case 'g':
+                            $number *= 1024 * 1024 * 1024;
+                            break;
+                        case 'm':
+                            $number *= 1024 * 1024;
+                            break;
+                        case 'k':
+                            $number *= 1024;
+                            break;
+                    }
+                }
+
+                $max_file_to_keep = ceil($number / $_POST['variables']['file_binlog_size']);
+
+
+                $db  = $this->di['db']->sql(DB_DEFAULT);
+                $sql = "REPLACE INTO binlog_max (`id_mysql_server`, `size_max`, `number_file_max`) VALUES ('".$_POST['mysql_server']['id']."', '".$number."', '".$max_file_to_keep."')";
+
+                $db->sql_query($sql);
+            }
+        }
     }
 
     public function max()
@@ -48,14 +86,15 @@ class Binlog extends Controller
         Debug::parseDebug($param);
 
         $id_mysql_server = $param[0];
-        $db = $this->di['db']->sql(DB_DEFAULT);
+        $db              = $this->di['db']->sql(DB_DEFAULT);
 
         $this->layout_name = false;
         $this->view        = false;
 
+
+
         Extraction::setDb($db);
         $res = Extraction::extract(array("variables::max_binlog_size"), array($id_mysql_server));
-
 
         $data = array();
         while ($ob   = $db->sql_fetch_object($res)) {
@@ -68,6 +107,80 @@ class Binlog extends Controller
         } else {
             echo "N/A";
         }
+    }
+
+    public function view($param)
+    {
+
+        Debug::parseDebug($param);
+
+        $db = $this->di['db']->sql(DB_DEFAULT);
+
+
+        $sql = "SELECT a.*, b.libelle as organization,c.*, a.id as id_mysql_server, d.*
+            FROM mysql_server a
+            INNER JOIN binlog_max d on a.id = d.id_mysql_server
+            INNER JOIN client b ON a.id_client = b.id
+            INNER JOIN environment c ON a.id_environment = c.id";
+
+
+        $res = $db->sql_query($sql);
+
+
+        $data['extra'] = array();
+        $mysql_server  = array();
+        while ($arr           = $db->sql_fetch_array($res, MYSQLI_ASSOC)) {
+
+
+            $remote    = Mysql::getDbLink($db, $arr['id_mysql_server']);
+            $db_remote = $this->di['db']->sql($remote);
+
+
+            $res2 = $db_remote->sql_query("show binary logs;");
+
+            while ($arr2 = $db_remote->sql_fetch_array($res2, MYSQLI_ASSOC)) {
+
+
+
+
+                $data['extra'][$arr['id_mysql_server']]['binary_logs']['file'][] = $arr2['Log_name'];
+                $data['extra'][$arr['id_mysql_server']]['binary_logs']['size'][] = $arr2['File_size'];
+            }
+
+
+            $mysql_server[] = $arr['id_mysql_server'];
+
+            $data['max_bin_log'][] = $arr;
+        }
+
+
+
+
+
+        Extraction::setDb($db);
+        $res = Extraction::extract(array("variables::max_binlog_size"), $mysql_server);
+
+
+        while ($ob = $db->sql_fetch_object($res)) {
+
+
+            $data['extra'][$ob->id_mysql_server]['max_binlog_size'] = $ob->value;
+        }
+
+        //debug($data);
+
+
+        $this->set('data', $data);
+    }
+
+    public function search($param)
+    {
+        
+    }
+
+    public function backup($param)
+    {
+
     }
 }
 //glyphicon glyphicon-list
--- application/view/Binlog/add.view.php
+++ application/view/Binlog/add.view.php
@@ -1,32 +1,36 @@
 <?php
+
 use Glial\Html\Form\Form;
 ?>
 
-<div class="panel panel-primary">
-    <div class="panel-heading">
-        <h3 class="panel-title"><?= __('Add an automate purge on binlog') ?></h3>
-    </div>
+
+
+<form action="" method="post">
+
 
     <div class="well">
         <div class="row">
             <div class="col-md-4">
                 <?= __("Serveur source"); ?>
-                <?php \Glial\Synapse\FactoryController::addNode("Common", "getSelectServerAvailable", array("database", "id_mysql_server__from", array("data-width" => "100%"))); ?>
+<?php \Glial\Synapse\FactoryController::addNode("Common", "getSelectServerAvailable", array("mysql_server", "id", array("data-width" => "100%"))); ?>
             </div>
 
             <div class="col-md-4">
                 <?= __("Size maximum allowed on server to all binlogs"); ?>
-                <?= Form::input("binlog_max", "size", array("class" => "form-control")) ?>
+<?= Form::input("binlog_max", "size", array("class" => "form-control")) ?>
             </div>
 
             <div class="col-md-2">
                 <?= __("Max binlog size"); ?>
-                <?= Form::input("binlog_max", "size", array("class" => "form-control")) ?>
+                <?= Form::input("variables", "max_binlog_size", array("class" => "form-control", "readonly" => "readonly")) ?>
+<?= Form::input("variables", "file_binlog_size", array("class" => "form-control", "type" => "hidden")) ?>
             </div>
 
             <div class="col-md-2"><br />
-                <button type="submit" class="btn btn-primary">Go</button>
+                <button type="submit" class="btn btn-primary">Add purge</button>
             </div>
         </div>
     </div>
-</div>
+
+
+</form>
