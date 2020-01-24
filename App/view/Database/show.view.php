c6b677f50d1c174f4d8264cb5ac3fa0721caefb8
Aurélien LEQUOY <pmacontrol@68koncept.com>
2020-01-24 14:32:14 +0100

    Amélioration de l'écran de la liste des base de données
    - Formatage des unitées
    - Ajout des totaux

--- App/view/Database/show.view.php
+++ App/view/Database/show.view.php
@@ -1,6 +1,13 @@
 <?php
 
-use App\Library\Display
+use App\Library\Display;
+
+function format($bytes, $decimals = 2)
+{
+    $sz     = ' KMGTP';
+    $factor = floor((strlen($bytes) - 1) / 3);
+    return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor))." ".@$sz[$factor]."o";
+}
 ?>
 <div class="panel panel-primary">
     <div class="panel-heading">
@@ -23,6 +30,14 @@ use App\Library\Display
         </tr>
 
         <?php
+        $total_data  = array();
+        $total_index = array();
+        $total_free  = array();
+        $total_table = array();
+        $total_row   = array();
+
+
+
         foreach ($data['database'] as $id_mysql_server => $elems) {
             foreach ($elems as $databases) {
                 $dbs = json_decode($databases['databases'], true);
@@ -38,18 +53,39 @@ use App\Library\Display
                             echo '<td>'.$db_attr['collation'].'</td>';
                             echo '<td>'.$engine.'</td>';
                             echo '<td>'.$row_format.'</td>';
-                            echo '<td>'.$details['size_data'].'</td>';
-                            echo '<td>'.$details['size_index'].'</td>';
-                            echo '<td>'.$details['size_free'].'</td>';
-                            echo '<td>'.$details['tables'].'</td>';
-                            echo '<td>'.$details['rows'].'</td>';
+                            echo '<td style="text-align:right;">'.format($details['size_data']).'</td>';
+                            echo '<td>'.format($details['size_index']).'</td>';
+                            echo '<td>'.format($details['size_free']).'</td>';
+                            echo '<td style="text-align:right;">'.$details['tables'].'</td>';
+                            echo '<td style="text-align:right;">'.$details['rows'].'</td>';
                             echo '<td>'.$details['table_collation'].'</td>';
                             echo '<tr>';
+
+
+                            $total_data[]  = $details['size_data'];
+                            $total_index[] = $details['size_index'];
+                            $total_free[]  = $details['size_free'];
+                            $total_table[] = $details['tables'];
+                            $total_row[]   = $details['rows'];
                         }
                     }
                 }
             }
         }
         ?>
+        <tr>
+            <th><?= __("TOTAL") ?></th>
+            <th><?= __("Database") ?></th>
+            <th><?= __("Charset") ?></th>
+            <th><?= __("Collation") ?></th>
+            <th><?= __("Engine") ?></th>
+            <th><?= __("Row format") ?></th>
+            <th><?= format(array_sum($total_data)) ?></th>
+            <th><?= format(array_sum($total_index)) ?></th>
+            <th><?= format(array_sum($total_free)) ?></th>
+            <th><?= array_sum($total_table) ?></th>
+            <th><?= array_sum($total_row) ?></th>
+            <th><?= __("Collations") ?></th>
+        </tr>
     </table>
 </div>
