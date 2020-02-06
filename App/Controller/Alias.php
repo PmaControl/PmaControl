c8b45147f6208a7459744c0dfdeabf85c07cac82
Aurélien LEQUOY <pmacontrol@68koncept.com>
2020-02-06 20:31:04 +0100

    imprived alias_dns with versioning table

--- App/Controller/Alias.php
+++ App/Controller/Alias.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,14 +9,20 @@ namespace App\Controller;
 
 use \Glial\Synapse\Controller;
 use \Glial\Sgbd\Sgbd;
+use App\Library\Debug;
+use App\Library\Extraction;
+use App\Library\System;
 
-class Alias extends Controller {
 
-    public function index() {
+class Alias extends Controller
+{
+
+    public function index()
+    {
         $db = Sgbd::sql(DB_DEFAULT);
 
-        $sql = "SELECT * FROM alias_dns a
-        ORDER BY dns";
+        $sql = "SELECT *,ROW_START,ROW_END FROM alias_dns a
+        ORDER BY dns, port";
 
         $res = $db->sql_query($sql);
 
@@ -31,4 +36,78 @@ class Alias extends Controller {
         $this->set('data', $data);
     }
 
+    /*
+     * met en relation un slave avec son master
+     * 
+     */
+    public function updateAlias($param)
+    {
+        Debug::parseDebug($param);
+        $db   = Sgbd::sql(DB_DEFAULT);
+        $list = Extraction::display(array("slave::master_host", "slave::master_port"));
+
+        $list_host = array();
+        foreach ($list as $masters) {
+            foreach ($masters as $master) {
+
+                $key = $master['master_host'].':'.$master['master_port'];
+
+                $host[$key]  = $master;
+                $list_host[] = $master['master_host'];
+            }
+        }
+
+        //Debug::debug($host);
+        Debug::debug($list_host);
+
+        $sql = "SELECT dns, port, id_mysql_server FROM `alias_dns`;";
+        $res = $db->sql_query($sql);
+
+        $all_dns = array();
+        while ($ob      = $db->sql_fetch_object($res)) {
+
+            $uniq           = $ob->dns.':'.$ob->port;
+            $all_dns[$uniq] = $ob->id_mysql_server;
+        }
+
+        Debug::debug($all_dns);
+
+        $sql = "SELECT id, ip, port FROM mysql_server";
+        $res = $db->sql_query($sql);
+
+        $mysql_server = array();
+        while ($ob           = $db->sql_fetch_object($res)) {
+            $uniq                = $ob->ip.':'.$ob->port;
+            $mysql_server[$uniq] = $ob->id;
+        }
+
+        foreach ($host as $dns) {
+            $uniq = $dns['master_host'].':'.$dns['master_port'];
+
+            if (!empty($mysql_server[$uniq])) {
+                continue;
+            }
+
+            if (!empty($all_dns[$uniq])) {
+                continue;
+            }
+
+            $ip   = System::getIp($dns['master_host']);
+            $uniq = $ip.':'.$dns['master_port'];
+
+            if (!empty($mysql_server[$uniq])) {
+
+                $alias_dns                                 = array();
+                $alias_dns['alias_dns']['id_mysql_server'] = $mysql_server[$uniq];
+                $alias_dns['alias_dns']['dns']             = $dns['master_host'];
+                $alias_dns['alias_dns']['port']            = $dns['master_port'];
+                $alias_dns['alias_dns']['destination']     = $ip;
+                $db->sql_save($alias_dns);
+            }
+        }
+
+        if (!IS_CLI) {
+            header("location: ".LINK."alias/index");
+        }
+    }
 }
--- App/view/Alias/index.view.php
+++ App/view/Alias/index.view.php
@@ -5,15 +5,22 @@
  * and open the template in the editor.
  */
 
+use App\Library\Display;
+
+echo '<a href="'.LINK.'alias/updateAlias/" class="btn btn-primary" style="font-size:12px"><span class="glyphicon glyphicon-refresh" style="font-size:12px"></span> Get aliases</a>';
+//echo ' ';
+//echo '<a href="'.LINK.'mysql/add/" class="btn btn-primary" style="font-size:12px"><span class="glyphicon glyphicon-plus" style="font-size:12px"></span> Add a custom alias</a>';
+echo '<br><br>';
 
 echo '<table class="table table-condensed table-bordered table-striped" id="table">';
 
 echo '<tr>';
 echo '<th>#</th>';
-echo '<th>Environment</th>';
 echo '<th>DNS</th>';
 echo '<th>Port</th>';
 echo '<th>Destination</th>';
+echo '<th>'.__('Linked to').'</th>';
+echo '<th>'.__('Since').'</th>';
 echo '</tr>';
 
 $i = 0;
@@ -23,14 +30,15 @@ foreach ($data['alia_dns'] as $alias) {
     $i++;
     echo '<tr>';
     echo '<td>'.$i.'</td>';
-    echo '<td>Environment</td>';
     echo '<td>'.$alias['dns'].'</td>';
     echo '<td>'.$alias['port'].'</td>';
     echo '<td>'.$alias['destination'].'</td>';
+    echo '<td>'.Display::srv($alias['id_mysql_server']);
+
+    $date_start = explode(".", $alias['ROW_START'])[0];
+
+    echo '<td>'.$date_start.'</td>';
     echo '</tr>';
 }
 
 echo '</table>';
-
-
-echo '';
