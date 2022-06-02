cf50fca238468d17cca76cfd38a58f58bea1e745
esysteme <timansl@hotmail.com>
2022-06-02 10:25:07 +0200

    add MCD v3

--- App/Controller/VirtualForeignKey.php
+++ App/Controller/VirtualForeignKey.php
@@ -24,6 +24,7 @@ class VirtualForeignKey extends Controller {
 
         //$id_mysql_server = $param[0];
 
+        $this->cleanUp($param);
         $this->autoId($param);
     }
 
@@ -35,6 +36,7 @@ class VirtualForeignKey extends Controller {
         $db = Mysql::getDbLink($id_mysql_server);
         $default = Sgbd::sql(DB_DEFAULT);
 
+
         $sql = "select TABLE_SCHEMA,TABLE_NAME, COLUMN_NAME 
 from information_schema.COLUMNS 
 where TABLE_SCHEMA NOT IN ('mysql', 'information_schema', 'performance_schema') 
@@ -44,11 +46,29 @@ and COLUMN_NAME != 'id' and COLUMN_NAME like 'id%'";
 
         while ($arr = $db->sql_fetch_array($res, MYSQLI_ASSOC)) {
 
-
-
             $schema_ref = $arr['TABLE_SCHEMA'];
             $table_ref = preg_replace('/(^id\_?)/i', '$2', $arr['COLUMN_NAME']);
             $table_id = "id";
+
+
+            if ($arr2 = $this->isTableExist(array($id_mysql_server, $schema_ref, $table_ref))) {
+                
+            } else {
+                if ($arr2 = $this->isTableExist(array($id_mysql_server, $schema_ref, $table_ref . "s"))) {
+                    
+                } else {
+                    if ($arr2 = $this->isTableExist(array($id_mysql_server, $schema_ref, $table_ref . "x"))) {
+                        
+                    } else {
+                        Debug::debug($table_ref, "Impossible de trouver la table");
+                        continue;
+                    }
+                }
+            }
+            
+            $schema_ref = $arr2['TABLE_SCHEMA'];
+            $table_ref = $arr2['TABLE_NAME'];
+            
 
             $sql2 = "SELECT count(1) as cpt
             from information_schema.COLUMNS     
@@ -73,16 +93,16 @@ and COLUMN_NAME != 'id' and COLUMN_NAME like 'id%'";
                 if ($ob->cpt === "1") {
                     $default->sql_save($virtual_foreign_key);
                 } else {
-                    
+
                     Debug::debug($ob->cpt, "count(1)");
-                    Debug::debug($virtual_foreign_key,"No found\n");
+                    Debug::debug($virtual_foreign_key, "No found\n");
                 }
             }
         }
     }
 
     /*
-     * CREATE TABLE `virtual_foreign_key` (
+     CREATE TABLE `virtual_foreign_key` (
       `id` int(11) NOT NULL AUTO_INCREMENT,
       `id_mysql_server` int(11) NOT NULL DEFAULT 0,
       `constraint_schema` varchar(64) NOT NULL DEFAULT '',
@@ -92,11 +112,67 @@ and COLUMN_NAME != 'id' and COLUMN_NAME like 'id%'";
       `referenced_table` varchar(64) NOT NULL DEFAULT '',
       `referenced_column` varchar(64) NOT NULL DEFAULT '',
       PRIMARY KEY (`id`),
+      UNIQUE KEY `id_mysql_server_2` (`id_mysql_server`,`constraint_schema`,`constraint_table`,`constraint_column`,`referenced_schema`,`referenced_table`,`referenced_column`),
       KEY `id_mysql_server` (`id_mysql_server`),
       CONSTRAINT `id_mysql_server_ibfk_1` FOREIGN KEY (`id_mysql_server`) REFERENCES `mysql_server` (`id`)
-      ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-     * 
-     * 
-     * 
+      ) ENGINE=InnoDB AUTO_INCREMENT=170 DEFAULT CHARSET=utf8mb4
+
      */
+
+    public function isTableExist($param) {
+
+        Debug::parseDebug($param);
+
+        $id_mysql_server = $param[0];
+        $table_name = $param[2];
+        $database_name = $param[1];
+
+        $db = Mysql::getDbLink($id_mysql_server);
+
+        $sql = "SELECT TABLE_SCHEMA, TABLE_NAME from `information_schema`.`tables` WHERE `TABLE_SCHEMA` = '" . $database_name . "' AND  LOWER(`TABLE_NAME`) = LOWER('" . $table_name . "');";
+        Debug::sql($sql);
+        $res = $db->sql_query($sql);
+
+        $nb_tables = $db->sql_num_rows($res);
+        if ($nb_tables > 1) {
+            Debug::parseDebug($nb_tables, "Nombre de tables");
+        }
+
+        while ($arr = $db->sql_fetch_array($res, MYSQLI_ASSOC)) {
+            Debug::debug($arr, "Table trouvé");
+            return true;
+        }
+
+        return false;
+    }
+
+    public function cleanUp($param) {
+
+        Debug::parseDebug($param);
+
+        $db = Sgbd::sql(DB_DEFAULT);
+        $sql = "TRUNCATE TABLE `virtual_foreign_key`";
+        $db->sql_query($sql);
+
+        Debug::sql($sql);
+    }
+
+    public function FindField($param) {
+        Debug::parseDebug($param);
+
+        $id_mysql_server = $param[0];
+        $database_name = $param[1];
+        $table_name = $param[2];
+        $field_name = $param[3];
+
+        $db = Mysql::getDbLink($id_mysql_server);
+
+        $sql = "SELECT count(1) as cpt
+        from information_schema.COLUMNS     
+        where TABLE_SCHEMA NOT IN ('mysql', 'information_schema', 'performance_schema') 
+        AND TABLE_SCHEMA = '" . $database_name . "'
+        AND TABLE_NAME = '" . $table_name . "'
+        AND COLUMN_NAME = '" . $field_name . "'";
+    }
+
 }
