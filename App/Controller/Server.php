8a46bdd1979624d8dec7ec16cd38a63b422b5243
esysteme <timansl@hotmail.com>
2022-08-10 01:15:20 +0200

    remove warning on insert link__mysql_server__tag

--- App/Controller/Server.php
+++ App/Controller/Server.php
@@ -213,7 +213,7 @@ class Server extends Controller
                  INNER JOIN client c on c.id = a.id_client
                  INNER JOIN environment d on d.id = a.id_environment
                  WHERE 1 ".self::getFilter()."
-                 ORDER by a.is_monitored DESC, c.is_monitored DESC, a.`is_acknowledged`, a.`is_available`, FIND_IN_SET(d.`id`, '1,19,2,16,3,7,4,2,6,8,5,17,18');";
+                 ORDER by a.is_monitored DESC, c.is_monitored DESC, a.`is_acknowledged`, a.`is_available`, FIND_IN_SET(d.`id`, '1,19,2,16,3,7,4,2,6,8,5,17,18'), a.display_name, a.ip;";
 
         $res = $db->sql_query($sql);
 
