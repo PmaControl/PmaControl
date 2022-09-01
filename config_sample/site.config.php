1b26c46a51c03aa5ec8e0cbb3147d0e76ce2c44b
Aurélien LEQUOY <timansl@hotmail.com>
2022-09-01 13:54:01 +0200

    test pre-commit 3

--- config_sample/site.config.php
+++ config_sample/site.config.php
@@ -1,6 +1,4 @@
 <?php
-
-
 if (!defined('SITE_URL')) {
     define("SITE_URL", "https://github.com/PmaControl/PmaControl");
 }
@@ -9,12 +7,19 @@ if (!defined('SITE_NAME')) {
     define("SITE_NAME", "PmaControl");
 }
 
+if (!defined('SITE_VERSION')) {
+    define("SITE_VERSION", "beta-v2.0.13");
+}
+if (!defined('SITE_LAST_UPDATE')) {
+    define("SITE_LAST_UPDATE", "2022-09-01");
+}
+
 
 if (!defined('SITE_LOGO')) {
         define('SITE_LOGO',"  _____                  _____            _             _
  |  __ \                / ____|          | |           | |
  | |__) | __ ___   __ _| |     ___  _ __ | |_ _ __ ___ | |
- |  ___/ '_  | |    / _ \| '_ \| __| '__/ _ \| |
+ |  ___/ '_ ` _ \ / _` | |    / _ \| '_ \| __| '__/ _ \| |
  | |   | | | | | | (_| | |___| (_) | | | | |_| | | (_) | |
  |_|   |_| |_| |_|\__,_|\_____\___/|_| |_|\__|_|  \___/|_|
                                                           
@@ -22,11 +27,3 @@ if (!defined('SITE_LOGO')) {
 ");
 }
 
-
-if (!defined('SITE_VERSION')) {
-    define("SITE_VERSION", "beta-v2.0.2");
-}
-
-if (!defined('SITE_LAST_UPDATE')) {
-    define("SITE_LAST_UPDATE", "2022-09-01-");
-}
