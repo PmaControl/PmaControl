6d84854b515445e83a90696b45388fb99eb6bd37
Aurélien LEQUOY <timansl@hotmail.com>
2022-09-01 13:36:17 +0200

    test pre commit 2

--- config_sample/site.config.php
+++ config_sample/site.config.php
@@ -11,10 +11,10 @@ if (!defined('SITE_NAME')) {
 
 
 if (!defined('SITE_LOGO')) {
-	define('SITE_LOGO',"  _____                  _____            _             _
+        define('SITE_LOGO',"  _____                  _____            _             _
  |  __ \                / ____|          | |           | |
  | |__) | __ ___   __ _| |     ___  _ __ | |_ _ __ ___ | |
- |  ___/ '_ ` _ \ / _` | |    / _ \| '_ \| __| '__/ _ \| |
+ |  ___/ '_  | |    / _ \| '_ \| __| '__/ _ \| |
  | |   | | | | | | (_| | |___| (_) | | | | |_| | | (_) | |
  |_|   |_| |_| |_|\__,_|\_____\___/|_| |_|\__|_|  \___/|_|
                                                           
@@ -24,9 +24,9 @@ if (!defined('SITE_LOGO')) {
 
 
 if (!defined('SITE_VERSION')) {
-    define("SITE_VERSION", "beta-v2.0.1");
+    define("SITE_VERSION", "beta-v2.0.2");
 }
 
 if (!defined('SITE_LAST_UPDATE')) {
-    define("SITE_LAST_UPDATE", "2022-09-01");
+    define("SITE_LAST_UPDATE", "2022-09-01-");
 }
