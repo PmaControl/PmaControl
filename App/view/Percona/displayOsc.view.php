f6e6d9d9d4bfb042bfe7ac8809e008ef45789b6c
Aurélien LEQUOY <timansl@hotmail.com>
2022-09-22 03:18:09 +0200

    split old and new for pt-osc

--- App/view/Percona/displayOsc.view.php
+++ App/view/Percona/displayOsc.view.php
@@ -4,44 +4,106 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+?>
 
-echo '<table class="table table-condensed table-bordered table-striped" id="table">';
 
-echo '<tr>';
-echo '<th>'.__('Top').'</th>';
-echo '<th>'.__('Environment').'</th>';
-echo '<th>'.__('Server').'</th>';
-echo '<th>'.__('Database').'</th>';
-echo '<th>'.__('Table').'</th>';
-echo '<th>'.__('Rows').'</th>';
 
-echo '<th>'.__('Data size').'</th>';
-echo '<th>'.__('Index size').'</th>';
-echo '<th>'.__('Free size').'</th>';
-echo '<th>'.__('Creation date').'</th>';
-echo '<th>'.__('Since').'</th>';
-echo '</tr>';
-$i = 0;
-foreach ($data['ptosc'] as $table) {
+<div class="panel panel-primary">
+    <div class="panel-heading">
 
-    $i++;
+        <h3 class="panel-title"><?= __('old') ?> pt-osc</h3>
+    </div>
+
+
+    <?php
+    echo '<table class="table table-condensed table-bordered table-striped" id="table">';
+
     echo '<tr>';
-    echo '<td>'.$i.'</td>';
-    echo '<td>';
-    echo '<big><span class="label label-'.$table['class'].'">'.$table['libelle'].'</span></big>';
-    echo '</td>';
-    echo '<td>'.$table['display_name'].'</td>';
-    echo '<td>'.$table['table_schema'].'</td>';
-    echo '<td>'.$table['table_name'].'</td>';
-    echo '<td>'.$table['table_rows'].'</td>';
-    echo '<td>'.$table['data_length'].'</td>';
-    echo '<td>'.$table['index_length'].'</td>';
-    echo '<td>'.$table['data_free'].'</td>';
-    echo '<td>'.$table['create_time'].'</td>';
-    echo '<td><span class="label label-warning">'.$table['days'].' '.__('days').'</span></td>';
+    echo '<th>'.__('Top').'</th>';
+    echo '<th>'.__('Environment').'</th>';
+    echo '<th>'.__('Server').'</th>';
+    echo '<th>'.__('Database').'</th>';
+    echo '<th>'.__('Table').'</th>';
+    echo '<th>'.__('Rows').'</th>';
+
+    echo '<th>'.__('Data size').'</th>';
+    echo '<th>'.__('Index size').'</th>';
+    echo '<th>'.__('Free size').'</th>';
+    echo '<th>'.__('Creation date').'</th>';
+    echo '<th>'.__('Since').'</th>';
     echo '</tr>';
-}
+    $i = 0;
+    foreach ($data['ptosc_old'] as $table) {
+
+        $i++;
+        echo '<tr>';
+        echo '<td>'.$i.'</td>';
+        echo '<td>';
+        echo '<big><span class="label label-'.$table['class'].'">'.$table['libelle'].'</span></big>';
+        echo '</td>';
+        echo '<td>'.$table['display_name'].'</td>';
+        echo '<td>'.$table['table_schema'].'</td>';
+        echo '<td>'.$table['table_name'].'</td>';
+        echo '<td>'.$table['table_rows'].'</td>';
+        echo '<td>'.$table['data_length'].'</td>';
+        echo '<td>'.$table['index_length'].'</td>';
+        echo '<td>'.$table['data_free'].'</td>';
+        echo '<td>'.$table['create_time'].'</td>';
+        echo '<td><span class="label label-warning">'.$table['days'].' '.__('days').'</span></td>';
+        echo '</tr>';
+    }
+
+    echo '</table>';
+    ?>
+</div>
 
 
 
-echo '</table>';
+<div class="panel panel-primary">
+    <div class="panel-heading">
+
+        <h3 class="panel-title"><?= __('currently running') ?></h3>
+    </div>
+
+
+    <?php
+    echo '<table class="table table-condensed table-bordered table-striped" id="table">';
+
+    echo '<tr>';
+    echo '<th>'.__('Top').'</th>';
+    echo '<th>'.__('Environment').'</th>';
+    echo '<th>'.__('Server').'</th>';
+    echo '<th>'.__('Database').'</th>';
+    echo '<th>'.__('Table').'</th>';
+    echo '<th>'.__('Rows').'</th>';
+
+    echo '<th>'.__('Data size').'</th>';
+    echo '<th>'.__('Index size').'</th>';
+    echo '<th>'.__('Free size').'</th>';
+    echo '<th>'.__('Creation date').'</th>';
+    echo '<th>'.__('Since').'</th>';
+    echo '</tr>';
+    $i = 0;
+    foreach ($data['ptosc_new'] as $table) {
+
+        $i++;
+        echo '<tr>';
+        echo '<td>'.$i.'</td>';
+        echo '<td>';
+        echo '<big><span class="label label-'.$table['class'].'">'.$table['libelle'].'</span></big>';
+        echo '</td>';
+        echo '<td>'.$table['display_name'].'</td>';
+        echo '<td>'.$table['table_schema'].'</td>';
+        echo '<td>'.$table['table_name'].'</td>';
+        echo '<td>'.$table['table_rows'].'</td>';
+        echo '<td>'.$table['data_length'].'</td>';
+        echo '<td>'.$table['index_length'].'</td>';
+        echo '<td>'.$table['data_free'].'</td>';
+        echo '<td>'.$table['create_time'].'</td>';
+        echo '<td><span class="label label-warning">'.$table['days'].' '.__('days').'</span></td>';
+        echo '</tr>';
+    }
+
+    echo '</table>';
+    ?>
+</div>
