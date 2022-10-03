9f3ebe39d6cbdcb71e7657b93f65f972f46c8259
Aurélien LEQUOY <timansl@hotmail.com>
2022-10-03 16:45:26 +0200

    add top and remove proxy

--- App/Controller/BI.php
+++ App/Controller/BI.php
@@ -24,7 +24,7 @@ class BI extends Controller
 
         $sql = "SELECT a.id,a.ip,a.port,a.display_name  FROM mysql_server a
             INNER JOIN environment b ON b.id = a.id_environment
-            WHERE is_available=1 and error = ''";
+            WHERE is_available=1 and is_proxy=0 and error = ''";
 
         if (!empty($environment)) {
             $sql .= " AND b.Libelle='".$environment."'";
@@ -32,10 +32,25 @@ class BI extends Controller
 
         $res = $db->sql_query($sql);
 
+        $i  = 0;
         while ($ob = $db->sql_fetch_object($res)) {
+
 
             $link  = Mysql::getDbLink($ob->id);
             $field = $param[0];
+
+            $sql3      = "SELECT @@global.read_only as read_only;";
+            $res3      = $link->sql_query($sql3);
+            $read_only = 0;
+            while ($ob3       = $link->sql_fetch_object($res3)) {
+
+                $read_only = $ob3->read_only;
+            }
+
+            if ($read_only == 1) {
+                continue;
+            }
+
 
             $sql2 = "SELECT TABLE_SCHEMA as TABLE_SCHEMA, TABLE_NAME as TABLE_NAME, COLUMN_NAME as COLUMN_NAME, COLUMN_TYPE as COLUMN_TYPE "
                 ."FROM information_schema.COLUMNS WHERE COLUMN_NAME = '".$field."'";
@@ -43,7 +58,8 @@ class BI extends Controller
             $res2 = $link->sql_query($sql2);
 
             while ($ob2 = $link->sql_fetch_object($res2)) {
-                echo $ob->display_name." ".$ob->ip.":".$ob->port." ".$ob2->TABLE_SCHEMA." ".$ob2->TABLE_NAME."\n";
+                $i++;
+                echo $i."\t".$ob->display_name."\t".$ob->ip.":".$ob->port."\t".$ob2->TABLE_SCHEMA." ".$ob2->TABLE_NAME."\n";
             }
         }
     }
