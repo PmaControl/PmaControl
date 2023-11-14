894b7d12eb734cebd4549168f308d47e8638f4d6
Aurélien LEQUOY <timansl@hotmail.com>
2023-11-14 18:38:08 +0100

    add global_variable and global_status

--- sql/incremental/mysql_variable.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `mysql_variable` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_mysql_server` int(11) NOT NULL,
-  `variable` varchar(128) NOT NULL,
-  `value` text NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_mysql_server` (`id_mysql_server`,`variable`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 WITH SYSTEM VERSIONING;
