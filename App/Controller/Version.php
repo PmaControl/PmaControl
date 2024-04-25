8a504f52a8fc0c27fa3aaa7d31894ddd0b6a009e
Volodymyr Barvinok <barvinok7771@gmail.com>
2024-04-25 14:12:18 +0200

    Add Versions page

--- App/Controller/Version.php
+++ App/Controller/Version.php
@@ -9,11 +9,20 @@ use \Glial\Sgbd\Sgbd;
 class Version extends Controller
 {
 
-    public function index($param)
+    public function index()
     {
-        $db = Sgbd::sql(DB_DEFAULT);
 
+        $name         = __("Version");                   
+        $this->title  = '<i class="fa fa-info-circle" style="font-size:32px"></i> '.$name;
+        $this->ariane = '> <i class="fa fa-question" style="font-size:16px" aria-hidden="true"></i> Help > <i class="fa fa-info-circle" style="font-size:16px"></i> '
+            .$name;
+
+
+        $db = Sgbd::sql(DB_DEFAULT);
         $sql ="SELECT * FROM `version`";
+        $data['version'] = $db->sql_fetch_yield($sql);
+
+        $this->set('data', $data);
 
 
     }
