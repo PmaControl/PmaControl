9900424214f99fbe3dc12a952af656d64a57bff9
Aurélien LEQUOY <timansl@hotmail.com>
2024-07-26 09:08:24 +0200

    add table for data model

--- App/Controller/Architecture.php
+++ App/Controller/Architecture.php
@@ -22,10 +22,24 @@ class Architecture extends Controller
 
         $db = Sgbd::sql(DB_DEFAULT);
 
+
+
+
+
         $sql = "SELECT c.id,c.display,c.height,c.`date`, b.id_architecture FROM mysql_server a
             INNER JOIN link__architecture__mysql_server b ON a.id = b.id_mysql_server
             INNER JOIN architecture c ON c.id = b.id_architecture
             WHERE 1 ".$this->getFilter()." AND c.height > 8 GROUP BY c.id ORDER BY c.height DESC,c.width DESC ";
+
+
+        $sql = "WITH LatestDot3Information AS (
+    SELECT MAX(id_dot3_information) AS max_id_dot3_information
+    FROM dot3_cluster
+)
+SELECT dg.*
+FROM dot3_graph dg
+JOIN dot3_cluster dc ON dg.id = dc.id_dot3_graph
+JOIN LatestDot3Information ldi ON dc.id_dot3_information = ldi.max_id_dot3_information;";
 
 
         //@TODO c.height > 8   => to fix on register table architecture on Dot
