e1ffc565d3223cce2ab3e804214ad197d673a4fc
Aurélien LEQUOY <timansl@hotmail.com>
2025-04-11 01:21:18 +0200

    v4.0.1

--- config_sample/site.config.php
+++ config_sample/site.config.php
@@ -8,13 +8,13 @@ if (!defined('SITE_NAME')) {
 }
 
 if (!defined('SITE_VERSION')) {
-    define("SITE_VERSION", "4.0");
+    define("SITE_VERSION", "4.0.1");
 }
 if (!defined('SITE_LAST_UPDATE')) {
-    define("SITE_LAST_UPDATE", "2024-05-28");
+    define("SITE_LAST_UPDATE", "2025-04-11");
 }
 if (!defined('SITE_BUILD')) {
-    define("SITE_BUILD", "9f1320108ac1eb5c9162064f3d7ddff5aefd9bc1");
+    define("SITE_BUILD", "1b4b6dc6aae1dd3d979dd9d0291c38dd2a884648");
 }
 if (!defined('SITE_LOGO')) {
         define('SITE_LOGO',"  _____                  _____            _             _
