458b42c68547971afb5b8ea0a8c7548df9f2179a
Aurélien LEQUOY <timansl@hotmail.com>
2025-06-07 00:50:20 +0200

    changed number version of mysql-sys

--- App/view/Mysqlsys/export.view.php
+++ App/view/Mysqlsys/export.view.php
@@ -6,21 +6,29 @@ if (!function_exists('generateDokuwikiTable')) {
 
     function generateDokuwikiTable($data) {
         // Récupération des colonnes (clés)
-        $columns = array_keys(current($data));
 
-        // Génération de l'en-tête du tableau
-        $output = "^ ID ^ " . implode(" ^ ", array_map('ucfirst', $columns)) . " ^\n";
+        if (count($data) > 1 )
+        {
+            $columns = array_keys(current($data));
 
-        // Génération des lignes du tableau
-        foreach ($data as $id => $row) {
-            $output .= "| $id ";
-            foreach ($columns as $col) {
-                $output .= "| ". "<nowiki>". $row[$col] . "</nowiki>";
+            // Génération de l'en-tête du tableau
+            $output = "^ ID ^ " . implode(" ^ ", array_map('ucfirst', $columns)) . " ^\n";
+
+            // Génération des lignes du tableau
+            foreach ($data as $id => $row) {
+                $output .= "| $id ";
+                foreach ($columns as $col) {
+                    $output .= "| ". "<nowiki>". $row[$col] . "</nowiki>";
+                }
+                $output .= "|\n";
             }
-            $output .= "|\n";
-        }
 
-        return $output;
+            return $output;
+        }
+        else
+        {
+            return "";
+        }
     }
 }
 
