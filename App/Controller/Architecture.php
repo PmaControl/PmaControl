deb19ee11ca03c5aa563036b686e5c49b05a7d97
Aurélien LEQUOY <timansl@hotmail.com>
2025-12-05 03:18:25 +0100

    Architecture: added organization selection and filtering using dot3_cluster__mysql_server to display only clusters containing servers linked to the chosen client, defaulting to all when none is selected.

--- App/Controller/Architecture.php
+++ App/Controller/Architecture.php
@@ -18,6 +18,25 @@ class Architecture extends Controller
     {
         Debug::parseDebug($param);
 
+        $selected_client = null;
+
+        // Parse URL parameters like client:libelle:["15"]
+        if (!empty($param[0]) && strpos($param[0], 'client:libelle:') === 0) {
+            $value = substr($param[0], strlen('client:libelle:'));
+            $json = json_decode($value, true);
+            if (is_array($json) && !empty($json)) {
+                $selected_client = intval($json[0]);
+                $_SESSION['architecture_client'] = $selected_client;
+            }
+        }
+
+        // Handle organization selection
+        if (!empty($_POST['client']['id'])) {
+            $selected_client = intval($_POST['client']['id']);
+            $_SESSION['architecture_client'] = $selected_client;
+        } elseif (!empty($_SESSION['architecture_client'])) {
+            $selected_client = $_SESSION['architecture_client'];
+        }
 
         /*
         $this->title  = '<i class="fa fa-object-group"></i> '.__("Architecture");
@@ -27,7 +46,7 @@ class Architecture extends Controller
 
         //https://masonry.desandro.com/events
         $this->di['js']->addJavascript(array("https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"));
-        
+
         $this->di['js']->code_javascript('
 
             $(".grid").masonry({
@@ -38,44 +57,45 @@ class Architecture extends Controller
 
         ');
 
-
-        
         $db = Sgbd::sql(DB_DEFAULT);
 
+        // Get clients for dropdown
+        $sql_clients = "SELECT id, libelle FROM client WHERE is_monitored = 1 ORDER BY libelle";
+        $data['client'] = $db->sql_fetch_yield($sql_clients);
+        $data['selected_client'] = $selected_client;
 
-        $sql = "WITH LatestDot3Information AS (
-    SELECT MAX(id_dot3_information)-1 AS max_id_dot3_information
-    FROM dot3_cluster
-)
-SELECT dg.*, (dg.height * dg.width) as area, dc.date_inserted as date_refresh
+        $sql = "SELECT dg.*, (dg.height * dg.width) as area, dc.date_inserted as date_refresh
 FROM dot3_graph dg
 JOIN dot3_cluster dc ON dg.id = dc.id_dot3_graph
-JOIN LatestDot3Information ldi ON dc.id_dot3_information = ldi.max_id_dot3_information
-ORDER BY  height DESC, width desc;";
+WHERE dc.id_dot3_information = (SELECT MAX(id_dot3_information)-1 FROM dot3_cluster)";
 
+        // Filter by selected organization if one is selected
+        $filter_conditions = "";
+        if (!empty($selected_client)) {
+            $filter_conditions = " AND EXISTS (
+                SELECT 1 FROM dot3_cluster__mysql_server dcms
+                INNER JOIN mysql_server ms ON ms.id = dcms.id_mysql_server
+                WHERE dcms.id_dot3_cluster = dc.id
+                AND ms.id_client = " . $db->sql_real_escape_string($selected_client) . "
+                AND ms.is_deleted = 0
+            )";
+        }
 
+        $sql .= $filter_conditions . " ORDER BY height DESC, width DESC;";
 
         $data['graphs'] = array();
 
         Debug::sql($sql);
         //@TODO c.height > 8   => to fix on register table architecture on Dot
 
-        /*         * *
-         * SELECT c.id,c.display FROM mysql_server a
-          INNER JOIN link__architecture__mysql_server b ON a.id = b.id_mysql_server
-          INNER JOIN architecture c ON c.id = b.id_architecture
-          WHERE 1 GROUP BY c.id
-         */
         $res = $db->sql_query($sql);
         while ($arr = $db->sql_fetch_array($res, MYSQLI_ASSOC))
         {
             $data['graphs'][] = $arr;
         }
-        
+
 
         $this->set('data', $data);
-
-
     }
 
     public function view($param)
